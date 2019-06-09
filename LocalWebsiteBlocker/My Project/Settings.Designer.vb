92d3ba304269691f44d31d47ee13713d1d350661
Francois Egner <francois.egner@outlook.com>
2019-06-10 01:56:32 +0200

    Changing german subjects into english ones

--- LocalWebsiteBlocker/My Project/Settings.Designer.vb
+++ LocalWebsiteBlocker/My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.LocalWebsiteBlocker.My.MySettings
+        Friend ReadOnly Property Settings() As Global.EzWebsiteBlocker.My.MySettings
             Get
-                Return Global.LocalWebsiteBlocker.My.MySettings.Default
+                Return Global.EzWebsiteBlocker.My.MySettings.Default
             End Get
         End Property
     End Module
